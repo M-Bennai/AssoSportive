58782c28676f755c3e6cc236f2ce3c575ee76a12
Massinisa Bennaï <massinisabennai@gmail.com>
2021-10-09 21:33:20 +0200

    update search route for gymnases and sportifs

--- controllers/gymnases.controller.js
+++ controllers/gymnases.controller.js
@@ -13,33 +13,49 @@ exports.findAll = (req, res) => {
     });
 };
 
-exports.find = (req, res) => {
-  const ville = req.params.Ville;
-  const nomGym = req.params.NomGymnase;
-  // const nomGymnase = req.params.NomGymnase
-
-  Gymnases.find({ Ville: ville })
-    .then((data) => {
-      if (data)
-        if (!data) res.status(404).send({ message: "Not found Ville" + Ville });
-        else res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({ message: "Error retrieving Ville" + Ville });
-    });
+exports.search = async (req, res) => {
+  let query = {};
+  if (req.query.keyword) {
+    query.$or = [
+      { Ville: { $regex: req.query.keyword } },
+      { NomGymnase: { $regex: req.query.keyword } },
+    ];
+  }
+  let gymn = await Gymnases.find(query);
+  return res.status(200).send({ message: "successfully fetched", data: gymn });
 };
+
+// exports.find = (req, res) => {
+//   const ville = req.params.Ville;
+//   const nomGym = req.params.NomGymnase;
+//   // const nomGymnase = req.params.NomGymnase
+
+//   Gymnases.find({ Ville: ville, NomGymnase: nomGymnase }) //|| { NomGymnase: nomGym })
+//     .then((data) => {
+//       if (data)
+//         if (!data) res.status(404).send({ message: "Not found Ville" + Ville });
+//         else res.send(data);
+//     })
+//     .catch((err) => {
+//       res.status(500).send({ message: "Error retrieving Ville" + Ville });
+//     });
+// };
 
 exports.findOne = (req, res) => {
   const idGymnase = req.params.id;
 
-  Gymnases.findById({ IdGymnase: idGymnase })
+  Gymnases.findOne({ _id: idGymnase })
     .then((data) => {
       if (!data)
-        res.status(404).send({ message: "Not found Gymnase with id " });
+        res
+          .status(404)
+          .send({ message: "Not found Gymnase with " + idGymnase });
       else res.send(data);
     })
     .catch((err) => {
-      res.status(500).send({ message: "Error retrieving Gymnase with id=" });
+      res
+        .status(500)
+        .send({ message: "Error retrieving Gymnase with " + idGymnase });
     });
 };
 
